1f725c9421824a5697c211c97acd1ec2732d705a
ticaki <59184294+ticaki@users.noreply.github.com>
2020-04-09 10:34:31 +0200

    Kommentare hinzugefügt

--- Telegram-Handler.js
+++ Telegram-Handler.js
@@ -1,21 +1,14 @@
 var telegramInstanz = 'telegram.0';
 
-var arrHeader = []; // {"Header":"", "result":,"sub":null,"cmd":,"text":""}
-var menuArr = [];
-var callbackCounter = 1400020000;
-var sameMenu=true;
-var currentMessage = '';
-var lastMessage = '';
-var tempMenuArr = [];
-var oldMessageID = []
-var lastMenu = [];
-var timeoutSameMenu = null;
 var inline_keyboard = false;
 const backText = 'Zurück';
 const cMenuTimeoutTime = 20*60*1000;
 
+/* erklärungen zum Menu (messageObj) sind unterhalb der Konfiguration */
+
+
 var messageObj = [
-    { //callback_data:'DWDUZWNINA#!§$'
+    {
         text: 'Menü',
         title: 'Menü',
         callback_data: 'Menu',
@@ -81,13 +74,84 @@ var messageObj = [
                     }
                 ], [
                     {
-                        text: 'Beleuchtung', title: 'Auswahl der Räume',command: getRoomsLightsSum, args:['state[role=switch](functions=beleuchtung)']
+                        text: 'Beleuchtung', title: 'Auswahl der Räume', command: getRoomsLightsSum, args:['state[role=switch](functions=beleuchtung)']
                     }
                 ]]
             }
         ]]
     }
 ]
+
+/* Konfigurationende */
+//123456
+
+/* Konfiguration des Menüs
+Vorlage
+{
+    text: 'Angezeiger Text im Knopf',
+    title: 'Verwendeter Nachrichtentext wenn dieser Knopf gedrückt wird.', // wird nur benötigt wenn ein neues Menü angezeigt wird
+    callback_data: 'eine eindeutige ID', //optional, das Script erstellt selbstständig solche Ids
+    submenu: [[]], // optional enthält die Vorlage [[Zeile1], [Zeile2]] oder [[Zeile1_Spalte1, Zeile1_Spalte2], [Zeile 2]]
+    new_menu: true, // optional für inline_keyboard die letzte Nachricht wird nicht mit neuen Daten editiert.
+    command: function, // optional vorhande oder selbsterstellte Funktion
+    args: [], // optional bis zu 5 Argumente, die der Funktion übergeben werden.
+}
+
+Eingebaute Funktionen für command und args
+
+command: switchState
+Schalte einen Boolean Schalter um
+args:['Objekt-ID']
+
+command: getTemperatur, args:['state[role=value.temperature][state.id=linkeddevices.0.Haus.*](functions=temperatursensor)']
+Gibt eine Liste von Raum + Temperatur zurück
+args: ist der Stringanteil eines Selectors
+
+command: getFeuchtigkeit, args:['state[state.id=linkeddevices.0.Haus.*][role=value.humidity](functions=luftfeuchtigkeit_sensor)'] }]
+Gibt eine Liste von Raum + Luftfeuchtigkeit zurück
+args: ist der Stringanteil eines Selectors
+
+getBatt, args:['Sensoren','channel[state.id=deconz.0.*.battery]',100] }]
+Gibt eine Liste von Batterieständen zurück mit common.name der übergeordneten Datenppunkte.
+args: 1. Letztes Wort der Überschrift (Ladestand der Sensoren)
+args: 2. ist der Stinganteil eines Selectors
+args: 3. Nur Battererien mit niedrigerem Ladestand werden ausgegeben.
+
+command: getRoomsLightsSum, args:['state[role=switch](functions=beleuchtung)']
+Gibt eine Menu mit einer Liste der Räume zurück mit der Anzahl der Lampen in diesen und der Anzahl der angeschaltenen Lampen. (Wohnzimmer (1/5)).
+dieses Menu ruft die Funktion getLightsOfRoom  auf.
+args: ist der Stringanteil eines Selectors
+
+command: getLightsOfRoom, args: [tRoom, selector, id = '', all = 0]
+Gibt eine Liste von Lampen aus dem Raum tRoom (enum.common.name) zurück.
+Lampen die Stats mit der Role level.brightness oder level.color.temperature enthalten, erhalten ein Detailfeld.
+Abhängig von der Lampenanzahl und der Anzahl der angeschalteten Lampen wird "Alle an" oder "Alle aus" dem Menu hinzugefügt.
+Diese Menu ruft sich selbst oder getDetailsOfLight
+args: 1. der Raum als enum.common.name
+args: 2. selector von getRoomsLightsSum
+args: 3. optional, die ObjectId der Lampe deren Status mit switchState umgeschaltet wird
+args: 4. optional, 0 = ignoriere diesen Parameter, 1 = schalte alle an, 2 = schalte alle aus.
+
+command: getDetailsOfLight,  args:[id, setId=null, value=0]
+Gibt ein Menü zurück mit Heller, Dunkler, Wärmer, Kälter zurück, mit dem die Lampe genau das tut. Ist für Deconz eingestellt.
+Dieses Menü ruft sich selbst auf.
+args: 1. Die ID zu einem der Datenpunkte der Lampe.
+args: 2. optional, der Datenpunkt der verändert wird
+args: 3. optional, der Wert der dem Datenpunkt 2. hinzuaddiert wird
+
+*/
+
+var arrHeader = []; // {"Header":"", "result":,"sub":null,"cmd":,"text":""}
+var menuArr = [];
+var callbackCounter = 1400020000;
+var sameMenu=true;
+var currentMessage = '';
+var lastMessage = '';
+var temporaeresMenuArray = [];
+var oldMessageID = []
+var lastMenu = [];
+var timeoutSameMenu = null;
+
 setCallbackData(messageObj);
 fillMenuArray(messageObj);
 
@@ -184,7 +248,7 @@ const mainTrigger = async function (obj) {
     lastMessage = currentMessage;
     let currMenu = [];
     currMenu = currMenu.concat(menuArr);
-    currMenu = currMenu.concat(tempMenuArr);
+    currMenu = currMenu.concat(temporaeresMenuArray);
     currentMessage = msg;
     let a = currMenu.findIndex((a) => { return a.callback_data === currentMessage; });
     if (a == -1) {
@@ -193,7 +257,7 @@ const mainTrigger = async function (obj) {
 
         return;
     }
-    if (a < menuArr.length) tempMenuArr = [];
+    if (a < menuArr.length) temporaeresMenuArray = [];
 
     let m = currMenu[a]; m.user = user;
     if (m.command !== undefined) {
@@ -329,34 +393,11 @@ function getFeuchtigkeit(selector) {
     return msg;
 }
 
-function getValuePerRoom(arr, id = false) {
-    var data = {};
-    arr.each((obj) => {
-        var rooms = getObject(obj.replace('.available', ''), 'rooms').enumIds;
-        if (rooms === undefined) return;
-        var temp;
-        if (id) temp = id;
-        else temp = getState(obj).val
-        var cName = getObject(obj).common.name;
-        rooms.forEach((room) => {
-            // workaround manche Geräte enthalten Räume die ich nicht entfernen kann.
-            var enumObj = getObject(room);
-            if (enumObj.common.members.findIndex((a) => {return obj.includes(a)}) == -1 ) return;
-            room = enumObj.common.name;
-            let r = room.de;
-            if (r === undefined) r = room;
-            if (data[r] === undefined) data[r] = {name: cName, values:[]};
-            data[r].values.push(temp);
-        });
-    });
-    return data;
-}
-
-function getLightsOfRoom(tRoom, selectArr, id = '') {
+function getLightsOfRoom(tRoom, selector, id = '', all = 0) {
     const prefix = '1-##**22';
     const prefixState = '#^#';
     var data = {};
-    selectArr.each((obj) => {
+    $(selector).each((obj) => {
         var rooms = getObject(obj.replace('.available', ''), 'rooms').enumIds;
         if (rooms === undefined) return;
         rooms.forEach((room) => {
@@ -374,6 +415,7 @@ function getLightsOfRoom(tRoom, selectArr, id = '') {
 
     });
     var result = {};
+    var allOn = {on:false, off:false};
     for (let obj in data) {
         if (result.submenu === undefined) {
             result.submenu = [];
@@ -381,15 +423,37 @@ function getLightsOfRoom(tRoom, selectArr, id = '') {
         }
         // vorwegnahme des Umschaltens, um mit getState() den Umschaltvorgang zu bestätigen wäre ein Timeout nötig.
         if (id == obj) data[obj].value = !data[obj].value;
+        else if ( all != 0 ) {
+            data[obj].value = (all ==1);
+            setState(obj, data[obj].value)
+        }
+        if (data[obj].value) allOn.off = true;
+        else allOn.on = true;
+
         let sId = obj.substring(0,obj.lastIndexOf('.'));
         let menu = [{text:data[obj].name + ' ' + (data[obj].value ? 'an 💡' : 'aus') , callback_data: prefix + obj}];
         if ($('state[role=level.brightness][state.id='+sId+'.*]').length > 0|| $('state[role=level.color.temperature][state.id='+sId+'.*]').length > 0) {
             menu.push({text:'Details' , callback_data: prefix + obj + 'details'});
         }
         result.submenu.push(menu);
-        if ( tempMenuArr.findIndex((a) => { return a.callback_data === prefix + obj }) == -1 ) {
-            tempMenuArr.push({text:data[obj].name, title:'', parent_data: lastMessage, callback_data: prefix + obj, command: getLightsOfRoom, args:[tRoom, selectArr, obj]});
-            tempMenuArr.push({text:'Details', title:'', parent_data: currentMessage, callback_data: prefix + obj + 'details', command: getDetailsOfLight, args:[obj]});
+        if ( temporaeresMenuArray.findIndex((a) => { return a.callback_data === prefix + obj }) == -1 ) {
+            temporaeresMenuArray.push({text:data[obj].name, title:'', parent_data: lastMessage, callback_data: prefix + obj, command: getLightsOfRoom, args:[tRoom, selector, obj]});
+            temporaeresMenuArray.push({text:'Details', title:'', parent_data: currentMessage, callback_data: prefix + obj + 'details', command: getDetailsOfLight, args:[obj]});
+        }
+    }
+    // füge dynamsche Alle an/aus hinzu nur wenn mehr als 1 Lampe vorhanden ist und wenn nicht alle Lampen den selben Status haben.
+    if ( result.submenu !== undefined && result.submenu.length > 1 && tRoom) {
+        let o1 = {text:'Alle an', callback_data: prefix + '#!' + tRoom};
+        let o2 = {text:'Alle aus', callback_data: prefix + '!#' + tRoom};
+        let menu = [];
+        if (allOn.on) menu.push(o1);
+        if (allOn.off) menu.push(o2);
+        result.submenu.unshift(menu);
+        if ( temporaeresMenuArray.findIndex((a) => { return a.callback_data === o1.callback_data }) == -1 ) {
+            temporaeresMenuArray.push({text: o1.text, title:'', parent_data: lastMessage, callback_data: o1.callback_data, command: getLightsOfRoom, args:[tRoom, selector, '', 1]});
+        }
+        if ( temporaeresMenuArray.findIndex((a) => { return a.callback_data === o2.callback_data }) == -1 ) {
+            temporaeresMenuArray.push({text: o2.text, title:'', parent_data: lastMessage, callback_data: o2.callback_data, command: getLightsOfRoom, args:[tRoom, selector, '', 2]});
         }
     }
     if (id) {
@@ -430,9 +494,9 @@ const getDetailsOfLight = async function (id, setId=null, value=0) {
         }
         let o1 = result.submenu[result.submenu.length-2][count];
         let o2 = result.submenu[result.submenu.length-1][count++];
-        if ( tempMenuArr.findIndex((a) => { return a.callback_data === o1.callback_data || a.callback_data === o2.callback_data;}) == -1 ) {
-            tempMenuArr.push({text:o1.text, title:'', parent_data: lastMessage, callback_data: o1.callback_data, command: getDetailsOfLight, args:[id, sobj, +25]});
-            tempMenuArr.push({text:o2.text, title:'', parent_data: lastMessage, callback_data: o2.callback_data, command: getDetailsOfLight, args:[id, sobj, -25]});
+        if ( temporaeresMenuArray.findIndex((a) => { return a.callback_data === o1.callback_data || a.callback_data === o2.callback_data;}) == -1 ) {
+            temporaeresMenuArray.push({text:o1.text, title:'', parent_data: lastMessage, callback_data: o1.callback_data, command: getDetailsOfLight, args:[id, sobj, +25]});
+            temporaeresMenuArray.push({text:o2.text, title:'', parent_data: lastMessage, callback_data: o2.callback_data, command: getDetailsOfLight, args:[id, sobj, -25]});
         }
     }
     return result;
@@ -453,14 +517,43 @@ function getRoomsLightsSum(selector) {
         let count = rooms[room].values.filter((a) => {return a==true}).length;
         if ( result.submenu === undefined ) result.submenu = [];
         result.submenu.push(
-            [{text:room +' (' + count + '/' + all_count + ')' , callback_data: prefix + room}]
+            [{text:room + ' (' + count + '/' + all_count + ')', callback_data: prefix + room}/*,
+        {text:(count == 0 ? 'An':'Aus') + ' (' + count + '/' + all_count + ')' , callback_data: prefix + room + (count == 0 ? '#an':'#aus'), temp:count}*/]
         );
         result.title = 'Auswahl der Räume';
-        if ( tempMenuArr.findIndex((a) => { return a.callback_data === prefix + room }) == -1 ) {
-            tempMenuArr.push({text:room, title:result.title, parent_data: currentMessage, callback_data: prefix + room, command: getLightsOfRoom, args:[room, $(selector), '']})
+        let o1 = result.submenu[result.submenu.length-1][0];
+        let o2 = result.submenu[result.submenu.length-1][1];
+        if ( temporaeresMenuArray.findIndex((a) => { return  a.callback_data === o1.callback_data }) == -1 ) {
+            temporaeresMenuArray.push({text:room, title:result.title, parent_data: currentMessage, callback_data: o1.callback_data, command: getLightsOfRoom, args:[room, selector, '']})
         }
+        //if ( temporaeresMenuArray.findIndex((a) => { return a.callback_data === o2.callback_data }) == -1 ) {
+        //    temporaeresMenuArray.push({text:room, title:'', parent_data: lastMessage, callback_data: o2.callback_data, command: getRoomsLightsSum, args:[selector, room, (o2.temp==0)]})
+        //}
     }
     return result;
+}
+
+function getValuePerRoom(arr, id = false, sRoom = '', value = false) {
+    var data = {};
+    arr.each((obj) => {
+        var rooms = getObject(obj.replace('.available', ''), 'rooms').enumIds;
+        if (rooms === undefined) return;
+        var temp;
+        if (id) temp = id;
+        else temp = getState(obj).val
+        var cName = getObject(obj.substring(0,obj.lastIndexOf('.'))).common.name;;
+        rooms.forEach((room) => {
+            // workaround manche Geräte enthalten Räume die ich nicht entfernen kann.
+            var enumObj = getObject(room);
+            if (enumObj.common.members.findIndex((a) => {return obj.includes(a)}) == -1 ) return;
+            room = enumObj.common.name;
+            let r = room.de;
+            if (r === undefined) r = room;
+            if (data[r] === undefined) data[r] = {name: cName, values:[]};
+            data[r].values.push(temp);
+        });
+    });
+    return data;
 }
 
 function getBatt(name, id, min) {
